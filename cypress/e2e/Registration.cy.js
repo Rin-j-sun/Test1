4e3128f6418bf855326f97761be2254c5e9ecb76
god <sabrina.sotnikova@gmail.com>
2024-03-20 11:21:35 +0700

    Регистрация с невалидной фамилией

--- cypress/e2e/Registration.cy.js
+++ cypress/e2e/Registration.cy.js
@@ -1,46 +1,46 @@
 describe('Tests Registration', ()=>{
-  it('Позитивная регистрация', ()=>{
-    cy.fixture('testData').then(data=>{
-      cy.log('Переход на сайт')
-      cy.visit(data.main_url)
-
-      cy.log('Переход на регистрацию')
-      cy.get('[href="/registration"] >.button').should('be.visible').click()
-      cy.url().should('include','registration')
-
-      cy.log('Ввод логина')
-      cy.get(':nth-child(1) > :nth-child(1) > .form-control--medium > .form-input--text').type(data.existent_login).should('have.value', data.existent_login)
-
-      cy.log('Ввод email')
-      cy.get('.form-input--email').type(data.existent_email).should('have.value', data.existent_email)
-
-      cy.log('Ввод пароля')
-      cy.get(':nth-child(3) > .form-control--medium > .form-input--password').type(data.existent_password).should('have.value', data.existent_password)
-
-      cy.log('Подтверждение пароля')
-      cy.get(':nth-child(4) > .form-control--medium > .form-input--password').type(data.existent_password).should('have.value', data.existent_password)
-
-      cy.log('Переход далее')
-      cy.get(':nth-child(4) > .button').should('be.visible').click()
-
-      cy.wait(2000)
-
-      cy.log('Ввод фамилии')
-      cy.get('[style=""] > :nth-child(1) > .form-control--medium > .form-input--text').type(data.existent_surname).should('have.value', data.existent_surname)
-
-      cy.log('Ввод имя')
-      cy.get(':nth-child(2) > .form-control--medium > .form-input--text').type(data.existent_name).should('have.value', data.existent_name)
-
-      cy.log('Ввод отчества')
-      cy.get(':nth-child(3) > .form-control--medium > .form-input--text').type(data.existent_patronymic).should('have.value', data.existent_patronymic)
-
-      cy.log('Создание аккаунта')
-      cy.get('.form__buttons > :nth-child(3)').should('be.visible').click()
-
-      cy.log('Проверка что перешли в личный кабинет')
-      cy.url().should('include','account/main')
-    })
-  })
+  // it('Позитивная регистрация', ()=>{
+  //   cy.fixture('testData').then(data=>{
+  //     cy.log('Переход на сайт')
+  //     cy.visit(data.main_url)
+  //
+  //     cy.log('Переход на регистрацию')
+  //     cy.get('[href="/registration"] >.button').should('be.visible').click()
+  //     cy.url().should('include','registration')
+  //
+  //     cy.log('Ввод логина')
+  //     cy.get(':nth-child(1) > :nth-child(1) > .form-control--medium > .form-input--text').type(data.existent_login).should('have.value', data.existent_login)
+  //
+  //     cy.log('Ввод email')
+  //     cy.get('.form-input--email').type(data.existent_email).should('have.value', data.existent_email)
+  //
+  //     cy.log('Ввод пароля')
+  //     cy.get(':nth-child(3) > .form-control--medium > .form-input--password').type(data.existent_password).should('have.value', data.existent_password)
+  //
+  //     cy.log('Подтверждение пароля')
+  //     cy.get(':nth-child(4) > .form-control--medium > .form-input--password').type(data.existent_password).should('have.value', data.existent_password)
+  //
+  //     cy.log('Переход далее')
+  //     cy.get(':nth-child(4) > .button').should('be.visible').click()
+  //
+  //     cy.wait(2000)
+  //
+  //     cy.log('Ввод фамилии')
+  //     cy.get('[style=""] > :nth-child(1) > .form-control--medium > .form-input--text').type(data.existent_surname).should('have.value', data.existent_surname)
+  //
+  //     cy.log('Ввод имя')
+  //     cy.get(':nth-child(2) > .form-control--medium > .form-input--text').type(data.existent_name).should('have.value', data.existent_name)
+  //
+  //     cy.log('Ввод отчества')
+  //     cy.get(':nth-child(3) > .form-control--medium > .form-input--text').type(data.existent_patronymic).should('have.value', data.existent_patronymic)
+  //
+  //     cy.log('Создание аккаунта')
+  //     cy.get('.form__buttons > :nth-child(3)').should('be.visible').click()
+  //
+  //     cy.log('Проверка что перешли в личный кабинет')
+  //     cy.url().should('include','account/main')
+  //   })
+  // })
 
   // it('Негативная регистрация', ()=>{
   //   cy.fixture('testData').then(data=>{
@@ -77,5 +77,49 @@ describe('Tests Registration', ()=>{
   //
   //   })
   // })
+
+  it('Негативная регистрация, с невалидной фамиией', ()=>{
+    cy.fixture('negativeRegistration').then(data=>{
+      cy.log('Переход на сайт')
+          cy.visit(data.main_url)
+
+          cy.log('Переход на регистрацию')
+          cy.get('[href="/registration"] >.button').should('be.visible').click()
+          cy.url().should('include','registration')
+
+          cy.log('Ввод логина')
+          cy.get(':nth-child(1) > :nth-child(1) > .form-control--medium > .form-input--text').type(data.existent_login).should('have.value', data.existent_login)
+
+          cy.log('Ввод email')
+          cy.get('.form-input--email').type(data.existent_email).should('have.value', data.existent_email)
+
+          cy.log('Ввод пароля')
+          cy.get(':nth-child(3) > .form-control--medium > .form-input--password').type(data.existent_password).should('have.value', data.existent_password)
+
+          cy.log('Подтверждение пароля')
+          cy.get(':nth-child(4) > .form-control--medium > .form-input--password').type(data.existent_password).should('have.value', data.existent_password)
+
+          cy.log('Переход далее')
+          cy.get(':nth-child(4) > .button').should('be.visible').click()
+
+          cy.wait(2000)
+
+          cy.log('Ввод невалидной фамилии')
+          cy.get('[style=""] > :nth-child(1) > .form-control--medium > .form-input--text').type(data.none_existent_surname).should('have.value', data.none_existent_surname)
+
+          cy.log('Проверка что появился элемент сигнализирующий об ошибке')
+          cy.get('.form-error > span').should('exist')
+
+          cy.log('Ввод имя')
+          cy.get(':nth-child(2) > .form-control--medium > .form-input--text').type(data.existent_name).should('have.value', data.existent_name)
+
+          cy.log('Ввод отчества')
+          cy.get(':nth-child(3) > .form-control--medium > .form-input--text').type(data.existent_patronymic).should('have.value', data.existent_patronymic)
+
+          cy.log('Проверка что создание аккаунта не возможно')
+          cy.get('.form__buttons > :nth-child(3)').should('be.disabled')
+
+        })
+  })
 
 })
